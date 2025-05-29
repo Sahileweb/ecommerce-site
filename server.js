d4ee9bbbae26ad84d34e9bfe5814d74e57a1bdb3
Sahileweb <sahil2003mundhe@gmail.com>
2025-05-29 22:32:15 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const userRoutes = require('./routes/userRoutes'); 
+
+const app = express();
+const PORT = 5000;
+
+
+mongoose.connect('mongodb://localhost:27017/myproductdb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
+
+// Middleware
+app.use(express.json());
+app.use('/api', userRoutes);
+
+app.use(express.static(path.join(__dirname, 'frontend')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'signup.html'));
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
